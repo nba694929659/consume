4d3e12680d24d5822b185db5bcea9e289a07750e
Jiangdi <694929659@qq.com>
2015-08-03 17:11:39 +0800

    提交版本基础
    项目框架

---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/8/3
+ * Time: 14:35
+ */
+namespace Home\Controller;
+use \Think\Controller;
+class UserController extends Controller{
+    public function index(){
+
+    }
+    public function register(){
+        echo 'register';
+    }
+    public function login_in(){
+        echo 'login';
+    }
+    public function login_out(){
+
+    }
+}
---
+++ Application/Home/Model/UserModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/8/3
+ * Time: 12:13
+ */
+namespace Home\Model;
+use Think\Model;
+class UserModel extends Model{
+    /***
+     * 注册
+     */
+    public function register(){
+
+    }
+}
